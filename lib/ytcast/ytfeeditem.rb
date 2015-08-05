c4cb43e09a40e2f810ca389b22bc718e6eab2e1b
Bruno Assis <brunoassis@gmail.com>
2015-08-04 23:29:59 -0300

    Added video_url and published_at attributes

--- lib/ytcast/ytfeeditem.rb
+++ lib/ytcast/ytfeeditem.rb
@@ -1,9 +1,21 @@
+require 'cgi'
+
 class YTFeedItem
-  attr_reader :title, :description, :video_id, :thumbnail
+  attr_reader :title, :description, :video_id, :thumbnail, :published_at
 
   def initialize(item_json)
     @internal_json = item_json
+    @local_video_url = nil
     parse_json
+  end
+
+  def video_url
+    if @local_video_url == nil
+      chosen_format = choose_format
+      video_url = get_video_url(chosen_format)
+      @local_video_url = video_url
+    end
+    @local_video_url
   end
 
   private
@@ -12,5 +24,23 @@ class YTFeedItem
     @description = @internal_json["snippet"]["description"]
     @video_id = @internal_json["snippet"]["resourceId"]["videoId"]
     @thumbnail = @internal_json["snippet"]["thumbnails"]["standard"]
+    @published_at = @internal_json["snippet"]["publishedAt"]
+  end
+
+  def choose_format
+    chosen_format = nil
+    stdin, stdout, stderr = Open3.popen3('youtube-dl', '-F', @video_id)
+    stdout.each_line do |line|
+      if line =~ /\(best\)/
+        chosen_format = line.match(/^\d+/).to_s
+        break
+      end
+    end
+    chosen_format
+  end
+
+  def get_video_url(chosen_format)
+    stdin, stdout, stderr = Open3.popen3('youtube-dl', '-g', @video_id, '-f', chosen_format)
+    CGI.escapeHTML stdout.gets.chomp!
   end
 end
