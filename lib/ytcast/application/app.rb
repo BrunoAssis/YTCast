21d7dd672563dadc9b1245b4de0998264a5d1a3c
Bruno Assis <brunoassis@gmail.com>
2015-08-04 23:53:12 -0300

    Removed unused namespaces

--- lib/ytcast/application/app.rb
+++ lib/ytcast/application/app.rb
@@ -11,8 +11,7 @@ class YTCastApp < Sinatra::Base
 
   get '/:playlist_id' do
     content_type :xml
-    #playlist_id = params[:playlist_id]
-    playlist_id = "PLX4wzH6YPQH3x25S7U6zp56wTopd1YYEN"
+    playlist_id = params[:playlist_id]
     feed = YTFeed.new(playlist_id)
     erb :feed, locals: {feed: feed}
   end
